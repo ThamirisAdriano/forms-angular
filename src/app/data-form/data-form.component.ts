725fc2035bdf79c37314b9d9b4c46848bbc4224b
Thamiris Adriano <thamirisadrianodarocha@MacBook-Thamiris.local>
2022-01-18 07:18:29 -0300

    validations

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,9 +1,10 @@
+import { FormValidations } from './../shared/form-validations';
 import { ConsultaCepService } from './../shared/services/consulta-cep.service';
 import { EstadoBr } from './../shared/models/estado-br';
 import { DropdownService } from './../shared/services/dropdown.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -49,7 +50,7 @@ export class DataFormComponent implements OnInit {
       email: [null, [Validators.required, Validators.email]],
 
       endereco: this.formBuilder.group({
-        cep: [null, Validators.required],
+        cep: [null, [Validators.required, FormValidations.cepValidator]],
         numero: [null, Validators.required],
         complemento: [null],
         rua: [null, Validators.required],
@@ -61,19 +62,75 @@ export class DataFormComponent implements OnInit {
       cargo: [null],
       tecnologias: [null],
       newsletterOp: [null],
-      termos:[null, Validators.pattern('true')] // utilizamos para validar uma expressão regular
+      termos:[null, Validators.pattern('true')],// utilizamos para validar uma expressão regular
+      frameworks: this.buildFrameworhs()
 
     });
-
     //Validators.minLength(3), Validators.maxLength(20)
   }
+
+  buildFrameworhs(){
+
+    const values = this.frameworks.map(v => new FormControl(false));
+    return this.formBuilder.array(values, FormValidations.minSelectedCheckboxes(1));
+
+  }
+
+  //CÓDIGO PESQUISADO :/
+
+  // minSelectedCheckboxes(min:number = 1) {
+  //   const validator: ValidatorFn = (formArray: AbstractControl) => {
+  //     if (formArray instanceof FormArray) {
+  //       const totalSelected = formArray.controls
+  //         .map((control) => control.value)
+  //         .reduce((prev, next) => (next ? prev + next : prev), 0);
+  //       return totalSelected >= min ? null : { required: true };
+  //     }
+
+  //     throw new Error('formArray is not an instance of FormArray');
+  //   };
+
+  //   return validator;
+  // }
+
+  // CÓDIGO INICIAL
+  // requiredMinCheckbox(min: number = 1){
+  //   const validator = (formArray: FormArray) => {
+  //     // LÓGICA PARA UTILIZAR PROGRAMAÇAO ESTRUTURADA
+  //     // const values = formArray.controls;
+  //     // let totalChecked = 0;
+  //     // for (let i=0; i<values.length; i++) {
+  //     //   if (values[i].value){
+  //     //     totalChecked +=1;
+  //     //   }
+  //     // }
+  //     //PROGRAMAÇAO FUNCIONAL (REATIVA)
+  //     const totalChecked = formArray.controls
+  //     .map(v => v.value)
+  //     .reduce((total, current) => current ? total + 1 : total, 0);
+  //     //reduce - utilizado para fazer somatórias
+  //     //(recebe dois parâmetros (valor corrente - se corrente for vdd retorno corrente + 1,)
+  //     //segundo parametro é o valor inicial - nesse caso = 0)
+
+  //     return totalChecked >= min ? null: { required: true};
+  //   };
+  //   return validator
+  // }
 
   onSubmit() {
     console.log(this.formulario);
 
+    let valueSubmit = Object.assign({}, this.formulario.value);
+
+    valueSubmit = Object.assign(valueSubmit, {
+      frameworks: valueSubmit.frameworks
+        .map((v: any, i: any) => v ? this.frameworks[i] : null)
+        .filter((v: any) => v !== null)
+    });
+
     if (this.formulario.valid){
       this.http.post('https://httpbin.org/post',
-      JSON.stringify(this.formulario.value))
+      JSON.stringify(valueSubmit))
       .subscribe(dados => {
         console.log(dados);
         //reseta o form
@@ -162,6 +219,10 @@ export class DataFormComponent implements OnInit {
 
   setarTecnologias(){
     this.formulario.get('tecnologias')?.setValue(['java', 'python', 'flutter'])
+  }
+
+  getFrameworksControls(){
+    return this.formulario.get('frameworks')? (<FormArray>this.formulario.get('frameworks')).controls : null;
   }
 
 
